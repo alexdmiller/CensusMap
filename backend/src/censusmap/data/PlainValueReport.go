ae32f1dfcdbd0d9eb01a79f1e1890ba698b8991b
Alex Miller <alexmiller@gmail.com>
2013-08-24 13:08:19 -0700

    PlainValue ParseConfig fills in required vars

--- backend/src/censusmap/data/PlainValueReport.go
+++ backend/src/censusmap/data/PlainValueReport.go
@@ -11,17 +11,20 @@ type PlainValueReport struct {
 }
 
 type PlainValueConfigFormat struct {
-  Kind string
   Vars map[string]string
 }
 
 func (r PlainValueReport) ParseConfig(config []byte) {
   parsed := new(PlainValueConfigFormat)
-  log.Printf("%s", config)
   err := json.Unmarshal(config, &parsed)
   if err != nil {
     log.Fatal(err)
   }
+  r.requiredVariables = make([]string, 0)
+  for _, code := range parsed.Vars {
+    r.requiredVariables = append(r.requiredVariables, code)
+  }
+  log.Printf("%v", r.requiredVariables)
 }
 
 func (r PlainValueReport) WriteFormattedReport(w io.Writer) {
--- backend/src/censusmap/data/PlainValueReport_test.go
+++ backend/src/censusmap/data/PlainValueReport_test.go
@@ -2,7 +2,7 @@ package data
 
 import ("testing")
 
-const dummyConfig string = `{"kind": "plain_value", "vars": {"Total Population": "B01003_001"}}`
+const dummyConfig string = `{"kind": "plain_value", "vars": {"Total Population": "B01003_001", "Other": "B02001_007"}}`
 
 func TestParseConfig(t *testing.T) {
   r := new(PlainValueReport)
