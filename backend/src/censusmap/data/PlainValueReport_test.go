bbbf0761287b2bfb788f3684f1d009ca8efb504f
Alex Miller <alexmiller@gmail.com>
2013-08-25 12:30:54 -0700

    tests for PlainValueReport.RequestAndParseData

--- backend/src/censusmap/data/PlainValueReport_test.go
+++ backend/src/censusmap/data/PlainValueReport_test.go
@@ -37,10 +37,11 @@ func TestParseConfig(t *testing.T) {
 func TestRequestAndParseData(t *testing.T) {
   _, codes := RequestLocationFromCoords(47.598755, -122.332764)
   r := newPlainValueReport(t)
-  result := r.RequestAndParseData(codes)
-  expected := `&{plain_value map[Other:0 Total Population:2134]}`
-  actual := fmt.Sprintf("%s", result)
-  if expected != actual {
-    t.Errorf("Expected %v but got %v", expected, actual)
+  result := r.RequestAndParseData(codes).(*PlainValueConfigFormat)
+  if result.Kind != "plain_value" {
+    t.Errorf(`Kind should be "plain_value", but was %s.`, result.Kind)
+  }
+  if len(result.Vars) != 2 {
+    t.Errorf("Vars should have 2 results, but instead has %d.", len(result.Vars))
   }
 }
