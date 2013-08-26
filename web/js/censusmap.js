7f6a64d565ec251f1143d42f7441579c3469899e
Alex Miller <alexmiller@gmail.com>
2013-08-26 00:11:59 -0700

    prettier interface for frontend report

--- web/js/censusmap.js
+++ web/js/censusmap.js
@@ -3,22 +3,26 @@ var currentMarker;
 
 $(document).ready(function() {
   var mapOptions = {
-    zoom: 1,
+    zoom: 4,
     center: new google.maps.LatLng(46.619, -120),
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
   map = new google.maps.Map(document.getElementById('map-canvas'),
     mapOptions);
   google.maps.event.addListener(map, 'click', onMapClick);
-  updateInfoBox(47.598755, -122.332764);
+  setMarker(46.619, -120);
 });
 
 function onMapClick(event) {
-  if (currentMarker) {
+  setMarker(event.latLng.mb, event.latLng.nb);
+}
+
+function setMarker(longitude, latitude) {
+   if (currentMarker) {
     currentMarker.setMap(null);
   }
-  currentMarker = new google.maps.Marker({position: event.latLng, map: map});
-  updateInfoBox(event.latLng.mb, event.latLng.nb);
+  currentMarker = new google.maps.Marker({position: new google.maps.LatLng(longitude, latitude), map: map});
+  updateInfoBox(longitude, latitude);
 }
 
 function updateInfoBox(latitude, longitude) {
@@ -31,9 +35,42 @@ function updateInfoBox(latitude, longitude) {
       long: longitude
     }
   }).done(function(response) {
-    console.log(response);
-    infobox.html(response);
+    infobox.html("");
+    response = JSON.parse(response);
+    $.each(response, function(i, report) {
+      switch (report.kind) {
+        case 'plain_value':
+        infobox.append(renderPlainValueReport(report));
+        break;
+        case 'composition':
+        infobox.append(renderCompositionReport(report));
+        break;
+      }
+    });
     infobox.removeClass('loading');
   });
 }
 
+function renderPlainValueReport(report) {
+  var wrapper = $("<div>");
+  wrapper.addClass("report");
+  $.each(report.vars, function(key, variable) {
+    variableWrapper = $("<div>");
+    variableWrapper.html(key + ": " + variable);
+    wrapper.append(variableWrapper);
+  });
+  console.log(wrapper);
+  return wrapper;
+}
+
+function renderCompositionReport(report) {
+  var wrapper = $("<div>");
+  wrapper.addClass("report");
+  $.each(report.parts, function(key, variable) {
+    variableWrapper = $("<div>");
+    variableWrapper.html(key + ": " + variable);
+    wrapper.append(variableWrapper);
+  });
+  console.log(wrapper);
+  return wrapper;
+}
